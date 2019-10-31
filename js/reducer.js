aa005ee7002ad1f0a1ccc1f09bc8c5d502a20539
fut0k0 <cccabell@icloud.com>
2019-10-31 11:40:56 -0700

    Completed lab

--- js/reducer.js
+++ js/reducer.js
@@ -2,25 +2,25 @@ let state
 
 function changeState(state={count: 0}, action) {
 
-		switch (action.type) {
-			case "INCREASE_COUNT":
-				return {count: state.count + 1};
-			default:
-				return state;
-		}
+	switch (action.type) {
+		case "INCREASE_COUNT":
+			return {count: state.count + 1};
+		default:
+			return state;
+	}
 
 }
 
 function render() {
 
-		document.body.textContent = state.count
+	document.body.textContent = state.count
 
 }
 
 function dispatch(action) {
 
-		state = changeState(state, action);
-		render();
+	state = changeState(state, action);
+	render();
 
 }
 

490bd50cdf75b4f7a8619d019bb6f43a749f2b07
Jeff Benton <jeff@frogrock.net>
2019-11-17 01:19:01 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,15 @@
-let state = {count: 0};
+let state;
 
-function changeState(state, action){
+function changeState(state = { count: 0 }, action) {
     switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
+
+        case 'INCREASE_COUNT':
+            return { count: state.count + 1 }
+
+        default:
+            return state;
     }
-  }
+}
 
 function dispatch(action){
     state = changeState(state, action)
@@ -17,3 +19,5 @@ function dispatch(action){
 function render(){
     document.body.textContent = state.count
 }
+
+dispatch({type: '@@INIT'})

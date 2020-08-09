6bb616b1c046915613e26d541b9dbbbacdf4f0b5
Roberto-Livi <robertomlivi@outlook.com>
2020-08-09 12:28:16 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,6 +1,6 @@
-let state = {count: 0};
+let state;
 
-function changeState(state, action){
+function changeState(state = {count: 0}, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}

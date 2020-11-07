2cc97148c657f6a32043a62f90158e35b3cefdea
ting682 <tchung682@gmail.com>
2020-11-07 08:26:47 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,11 +1,14 @@
-let state = {count: 0};
+let state;
 
-function changeState(state, action){
+function changeState(state = { count: 0 }, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
-      default:
+      default: {
+        
         return state;
+      }
+        
     }
   }
 
@@ -17,3 +20,5 @@ function dispatch(action){
 function render(){
     document.body.textContent = state.count
 }
+
+dispatch({ type: '@@INIT' })

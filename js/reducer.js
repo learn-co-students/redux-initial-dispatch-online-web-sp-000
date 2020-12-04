2e7128f6a2d44f074ed91e2e8a8b5dfde4361af4
David <50710431+dmaster18@users.noreply.github.com>
2020-12-04 16:56:15 -0500

    Commit #1

--- js/reducer.js
+++ js/reducer.js
@@ -2,12 +2,14 @@ let state = {count: 0};
 
 
 function changeState(state, action){
+    
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
       default:
         return state;
     }
+    
   }
 
 function dispatch(action){
